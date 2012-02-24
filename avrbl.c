c9bcd6f514fd3489d43ed6fb4d9eb54297295063
Rich_Testardi <rich@testardi.com>
2012-02-24 13:26:57 -0700

    accept unaligned bytes at the *end* (only) of a linker segment

--- avrbl.c
+++ avrbl.c
@@ -212,7 +212,9 @@ void
 avrbl_message(byte *request, int size)
 {
     uint i;
+    uint32 temp;
     uint nbytes;
+    uint fbytes;
     uint address;
     int rawi;
     byte raw[64];
@@ -290,11 +292,27 @@ avrbl_message(byte *request, int size)
                 flash_erase_pages((void *)FLASH_START, FLASH_BYTES/FLASH_PAGE_SIZE);
                 erased = true;
             }
+
+            // we program word-at-a-time and then an optional fragment of a word
             ASSERT(((uintptr)(request+10)&3)==0);
             nbytes = ((request[1])<<8)|(request[2]);
+            fbytes = nbytes & 3;
+            nbytes -= fbytes;
+
+            // program the words
             ASSERT((nbytes&3)==0);
             flash_write_words((uint32 *)(FLASH_START+load_address), (uint32 *)(request+10), nbytes/4);
             load_address += nbytes;
+
+            // if there was a fragment of a word left to program...
+            if (fbytes) {
+                // program the fragment
+                temp = -1;
+                memcpy(&temp, (uint32 *)(request+10+nbytes), fbytes);
+                flash_write_words((uint32 *)(FLASH_START+load_address), &temp, 1);
+                load_address += fbytes;
+                // N.B. we'll ASSERT if we attempt to program words after a fragment (illegal)
+            }
             break;
         case CMD_READ_FLASH_ISP:
             nbytes = ((request[1])<<8)|(request[2]);
